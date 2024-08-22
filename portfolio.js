36d67aadfd1ba10b5a7f5868c9976bf63405d3c2
ShubH9604 <153529720+ShubH9604@users.noreply.github.com>
2024-08-22 22:32:27 +0530

    Delete portfolio.js

--- portfolio.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const skills = document.querySelectorAll('.skill');
-    skills.forEach((skill, index) => {
-        skill.style.animationDelay = `${index * 0.2}s`;
-    });
-});
